f1648e1b63957e0bdcfca348c365bd2bebbc2b24
Nikita Buts <42112029+nikitabuts@users.noreply.github.com>
2020-04-20 20:33:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -79,5 +79,4 @@ def predict(vocab=vocab, word_to_index=word_to_index,
             })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
